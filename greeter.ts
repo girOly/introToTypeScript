5bbe9295c8f7002d57e539a34cd37239ad5042a2
girOly <girardob@gmail.com>
2019-09-19 14:39:29 -0400

    finished tutorial

--- greeter.ts
+++ greeter.ts
@@ -1,12 +1,22 @@
-const greeter = function(person: Person) {
-  return "Hello, " + person.firstName + " " + person.lastName;
-};
-
-let user = { firstName: "Lauren", lastName: "Ipsum" };
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
 
 interface Person {
   firstName: string;
   lastName: string;
 }
 
+const greeter = function(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+};
+
+let user = { firstName: "Lauren", lastName: "Ipsum" };
 document.body.textContent = greeter(user);
